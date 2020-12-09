28a906aecee8915e5b55f3d89fc4d04ed95c1722
pollfly <75068813+pollfly@users.noreply.github.com>
2020-12-09 12:02:58 +0200

    add product and company object

---
+++ product object.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+class Food(Product):
+    def __init__(self, name, company):
+        super.__init__(self, name, company)
+        self.ingredients = []
+
+    def add_ingredients
