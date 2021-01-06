f3314f7b9c0d1cea70b782d93d7e62c9d34be367
pollfly <75068813+pollfly@users.noreply.github.com>
2021-01-06 18:59:22 +0200

    add comp function

--- company_app/flask_app_server.py
+++ company_app/flask_app_server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from company_database_functions import lookup_comp
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/check_company/<myCompany>")
+def company_check(myCompany):
+    return lookup_comp(myCompany)
 
 if __name__ == "__main__":
     app.run(debug=True)
