a6b569cfe021076a3d9af8a05ff799c63d9bdc1d
pollfly <75068813+pollfly@users.noreply.github.com>
2021-01-20 15:25:15 +0200

    connect search to result

--- company_app/flask_app_server.py
+++ company_app/flask_app_server.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, request, redirect, render_template
 from company_database_functions import lookup_comp
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        comp = request.form["company"]
+        return redirect(url_for("company_check", myCompany=comp))
+    else:
+        return render_template("home.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/check_company/<myCompany>")
+@app.route("/<myCompany>")
 def company_check(myCompany):
     return lookup_comp(myCompany)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
