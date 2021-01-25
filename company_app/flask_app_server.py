de989338c5d7cda3ccbfcb29195ccf527b754b08
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 22:14:04 +0200

    Update flask_app_server.py

--- company_app/flask_app_server.py
+++ company_app/flask_app_server.py
@@ -4,6 +4,15 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
+# P.Z: maybe it will be prettier to saparate it to two different functions and remove the if.
+# @app.route("/", methods=["POST"])
+# def home_post():
+#   comp = request.form["company"]
+#   return redirect(url_for("company_check", myCompany=comp))
+#
+# @app.route("/", methods=["GET"])
+# def home2_get():
+#   return render_template("home.html")
 
 @app.route("/", methods=["POST","GET"])
 def home():
@@ -12,7 +21,6 @@ def home():
         return redirect(url_for("company_check", myCompany=comp))
     else:
         return render_template("home.html")
-
 
 @app.route("/<myCompany>")
 def company_check(myCompany):
