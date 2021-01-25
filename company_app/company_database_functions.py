ce50250bcc33f61585046660aa9a2103b0522a83
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 22:32:28 +0200

    Update company_database_functions.py

--- company_app/company_database_functions.py
+++ company_app/company_database_functions.py
@@ -1,13 +1,16 @@
 import sqlite3
 
 
+# P.Z: I would recommend to rename to "print_all_companies" or something more specific. 
 def show_all():
+    # P.Z: Coping code is the best way to generate codes. I would recommend to take those few lines and maybe returns a curser or even executes a statement.
     conn1 = sqlite3.connect('../company.db')
     c = conn1.cursor()
     c.execute("SELECT rowid, * FROM companies")
     items = c.fetchall()
     for item in items:
         print(item)
+    # P.Z: There is no need to commit nothing is changed.
     conn1.commit()
     conn1.close()
 
@@ -19,7 +22,7 @@ def add_company(name, location, year, products, history):
     conn1.commit()
     conn1.close()
 
-
+#P.Z: Same here, maybe rename to "delete_company"
 def delete_one(id):
     conn1 = sqlite3.connect('../company.db')
     c = conn1.cursor()
@@ -28,6 +31,7 @@ def delete_one(id):
     conn1.close()
 
 
+# P.Z: You are not paying for characters, please replace "comp" with "company"
 def lookup_comp(comp=None):
     if not comp:
         comp = input("Search for a company: ")
@@ -52,6 +56,7 @@ def lookup_comp(comp=None):
             letters = comp[:2]
             c.execute("SELECT name FROM companies WHERE name LIKE (?)", (f'{letters}%',))
             items = c.fetchall()
+            # P.Z: here you are checking "if not items", 5 lines after you are checking "if items" but items is not changed. Maybe you can use if-else?
             if not items:
                 return "<h2>Company not found</h2> <br> "
             stringify = ""
