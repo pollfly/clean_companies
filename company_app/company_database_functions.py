a6dfafb1afdbae9fb90b0e9bd0b684c35ee39aa6
pollfly <75068813+pollfly@users.noreply.github.com>
2021-01-26 19:15:52 +0200

    add table, crawler info, clean

--- company_app/company_database_functions.py
+++ company_app/company_database_functions.py
@@ -1,26 +1,24 @@
 import sqlite3
 
 
-def show_all():
+def get_all_companies():
     conn1 = sqlite3.connect('../company.db')
     c = conn1.cursor()
     c.execute("SELECT rowid, * FROM companies")
-    items = c.fetchall()
-    for item in items:
-        print(item)
-    conn1.commit()
+    items = set(c.fetchall())
     conn1.close()
+    return items
 
 
-def add_company(name, location, year, products, history):
+def add_company(name, location, products, history):
     conn1 = sqlite3.connect('../company.db')
     c = conn1.cursor()
-    c.execute("INSERT INTO companies VALUES (?,?,?,?,?)", (name, location, year, products, history))
+    c.execute("INSERT INTO companies VALUES (?,?,?,?)", (name, location, products, history))
     conn1.commit()
     conn1.close()
 
 
-def delete_one(id):
+def delete_company(id):
     conn1 = sqlite3.connect('../company.db')
     c = conn1.cursor()
     c.execute("DELETE FROM companies WHERE rowid = (?)", id)
@@ -28,7 +26,7 @@ def delete_one(id):
     conn1.close()
 
 
-def lookup_comp(comp=None):
+def lookup_company(comp=None):
     if not comp:
         comp = input("Search for a company: ")
     conn1 = sqlite3.connect('../company.db')
@@ -37,25 +35,30 @@ def lookup_comp(comp=None):
               (comp, comp.upper(), comp.lower(), comp.title()))
     items = c.fetchall()
     if items:
-        for comp, location, year, products, history in items:
+        for comp, location, products, history in items:
+            history = history.replace("\n", "<br>")
+            print(history)
             return f"""
             <h1> <strong>Company:</strong><big style="color: blue"> {comp}</big></h1> 
             <hr/>
             <p> <strong>Location:</strong> {location} </p>
-            <p> <strong>Founded:</strong> {year} </p>
             <p> <strong>Products:</strong> {products} </p>
             <p> <strong>History:</strong> {history} </p>"""
     else:
         if len(comp) < 2:
-            return "<h2>Company not found</h2> <br> "
+            return "<h1>Company not found</h1> <br> "
         else:
             letters = comp[:2]
             c.execute("SELECT name FROM companies WHERE name LIKE (?)", (f'{letters}%',))
             items = c.fetchall()
+            # P.Z: here you are checking "if not items", 5 lines after you are checking "if items" but items is not
+            # changed. Maybe you can use if-else?
             if not items:
                 return "<h2>Company not found</h2> <br> "
             stringify = ""
-            for item in items:
-                stringify += f"<form action='/{item[0]}'> <input type='submit' value={item[0]}> </form>"
+            list_items = [list(i) for i in items]
+            for item in list_items:
+                item[0] = item[0].strip()
+                stringify += f"<form action='/{item[0]}'> <input type='submit' value='{item[0]}'> </form>"
             if items:
                 return f"<h1>Do you mean one of these companies?<h1/>{stringify}<hr/>"
