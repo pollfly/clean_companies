7b94ff8ab016332be7d9c9b900900b23b92e4167
pollfly <75068813+pollfly@users.noreply.github.com>
2021-02-01 00:00:56 +0200

    fix numbers bug

--- company_app/company_database_functions.py
+++ company_app/company_database_functions.py
@@ -4,7 +4,7 @@ import config
 
 def connect_to_database():
     config.conn1 = sqlite3.connect(config.company_database)
-    config.c = config.conn1.cursor()
+    config.cursor = config.conn1.cursor()
 
 
 def commit_close_database_connection():
@@ -21,28 +21,29 @@ def get_all_companies_info(comp_id=False, name=True, country_founded=True, produ
         if boolean:
             stringify += f"{info_type}, "
     stringify = stringify[:-2]
-    config.c.execute(f"SELECT {stringify} FROM companies")
-    items = set(config.c.fetchall())
+    config.cursor.execute(f"SELECT {stringify} FROM companies")
+    items = set(config.cursor.fetchall())
     config.conn1.close()
     return items
 
 
 def add_company(name, location, products, history):
     connect_to_database()
-    config.c.execute("INSERT INTO companies VALUES (?,?,?,?)", (name, location, products, history))
+    config.cursor.execute("INSERT INTO companies VALUES (?,?,?,?)", (name, location, products, history))
     commit_close_database_connection()
 
 
 def delete_company(id_num):
     connect_to_database()
-    config.c.execute("DELETE FROM companies WHERE rowid = (?)", (id_num,))
+    config.cursor.execute("DELETE FROM companies WHERE rowid = (?)", (id_num,))
     commit_close_database_connection()
 
 
 def get_comp_rowid(company_name):
     connect_to_database()
-    config.c.execute("SELECT rowid, name FROM companies WHERE name = (?) ", (company_name,))
-    items = config.c.fetchall()
+    config.cursor.execute("SELECT rowid, name FROM companies WHERE name = (?) ", (company_name,))
+    items = config.cursor.fetchall()
+    config.conn1.close()
     return items
 
 
@@ -50,13 +51,12 @@ def lookup_company(company=None):
     if not company:
         company = input("Search for a company: ")
     connect_to_database()
-    config.c.execute("SELECT * FROM companies WHERE name = (?) or name = (?) or name = (?) or name = (?) ",
-                     (company, company.upper(), company.lower(), company.title()))
-    items = config.c.fetchall()
+    config.cursor.execute("SELECT * FROM companies WHERE name = (?) or name = (?) or name = (?) or name = (?)",
+                          (company, company.upper(), company.lower(), company.title()))
+    items = config.cursor.fetchall()
     if items:
         for company, location, products, history in items:
             history = history.replace("\n", "<br>")
-            print(history)
             return f"""
             <h1> <b>Company:</b><big style="color: blue"> {company}</big></h1> 
             <hr/>
@@ -67,9 +67,8 @@ def lookup_company(company=None):
         if len(company) < 2:
             return "<h1>Company not found</h1> <br> "
         else:
-            letters = company[:2]
-            config.c.execute("SELECT name FROM companies WHERE name LIKE (?) or (?)", (f"%{company}%", f"{letters}%"))
-            items = config.c.fetchall()
+            config.cursor.execute("SELECT name FROM companies WHERE name LIKE (?) ORDER BY name ASC", (f"%{company}%",))
+            items = config.cursor.fetchall()
             if not items:
                 return "<h2>Company not found</h2> <br> "
             else:
@@ -79,4 +78,4 @@ def lookup_company(company=None):
                     item[0] = item[0].strip()
                     stringify += f"""<form action="/{item[0]}"> <input type="submit" value="{item[0]}"> </form>"""
                 if items:
-                    return f"<h1>Do you mean one of these companies?<h1/>{stringify}<hr/>"
+                    return f"<h1>Do you mean one of these companies?<h1/>{stringify}"
